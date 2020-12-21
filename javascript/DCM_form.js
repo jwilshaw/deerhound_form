a3d2aa872f94a2fb27a4a3ae26bd3b51e1ef55c8
jwilshaw <44175376+jwilshaw@users.noreply.github.com>
2020-12-21 21:13:31 +0000

    Reset count

--- javascript/DCM_form.js
+++ javascript/DCM_form.js
@@ -3,7 +3,7 @@ const countEl = document.getElementById('counter');
 updateVisitCount()
 
 function updateVisitCount() {
-  fetch('https://api.countapi.xyz/update/deerhoundgenetic/deerhoundnumber/?amount=1')
+  fetch('https://api.countapi.xyz/update/deerhoundgenetics.com/deerhoundcount/?amount=1')
   .then(res => res.json())
   .then(res => {
     countEl.innerHTML = res.value;
